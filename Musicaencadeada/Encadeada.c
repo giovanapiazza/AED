5990444497ac58509fa7c6fb31f023f1c465c3f1
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-05-05 21:43:49 -0300

    Update Encadeada.c

--- Musicaencadeada/Encadeada.c
+++ Musicaencadeada/Encadeada.c
@@ -159,94 +159,13 @@ void criaListaComDados(struct desc_LDE *lista) {
     printf("\nLista criada com %d músicas!\n", lista->tamanho);
 }
 
-// musicasmain.c
-int main(void) {
-    struct desc_LDE *lista = criaDescritor();
-    int op;
-
-    do {
-        printf("\n--- MENU ---\n");
-        printf("1 - Criar lista de músicas\n");
-        printf("2 - Inserir uma nova música\n");
-        printf("3 - Remover uma música da lista\n");
-        printf("4 - Procurar por uma música\n");
-        printf("5 - Imprimir lista de músicas\n");
-        printf("0 - Encerrar\n");
-        printf("Opção: ");
-        scanf("%d", &op);
-        setbuf(stdin, NULL);
-
-        switch(op) {
-            case 0:
-                printf("Encerrando...\n");
-                break;
-
-            case 1:
-                criaListaComDados(lista);
-                break;
-
-            case 2: {
-                struct musica *m = malloc(sizeof(struct musica));
-                int pos;
-
-                printf("Título: ");
-                fgets(m->titulo, sizeof(m->titulo), stdin);
-                m->titulo[strcspn(m->titulo, "\n")] = '\0';
-
-                printf("Artista: ");
-                fgets(m->artista, sizeof(m->artista), stdin);
-                m->artista[strcspn(m->artista, "\n")] = '\0';
-
-                printf("Letra: ");
-                fgets(m->letra, sizeof(m->letra), stdin);
-                m->letra[strcspn(m->letra, "\n")] = '\0';
-
-                printf("Código: ");
-                scanf("%d", &m->codigo);
-                setbuf(stdin, NULL);
-
-                printf("Número de execuções: ");
-                scanf("%d", &m->execucoes);
-                setbuf(stdin, NULL);
-
-                printf("Posição para inserir (0 a %d): ", lista->tamanho);
-                scanf("%d", &pos);
-                setbuf(stdin, NULL);
-
-                struct nodo_LDE *novo = criaNodo(m);
-                insere(lista, novo, pos);
-                break;
-            }
-
-            case 3: {
-                int pos;
-                printf("Posição da música a remover: ");
-                scanf("%d", &pos);
-                setbuf(stdin, NULL);
-                struct nodo_LDE *rem = removeLista(lista, pos);
-                if (rem) free(rem->info);
-                free(rem);
-                break;
-            }
-
-            case 4: {
-                char nome[256];
-                printf("Digite o nome da música: ");
-                fgets(nome, sizeof(nome), stdin);
-                nome[strcspn(nome, "\n")] = '\0';
-                procurar(lista, nome);
-                break;
-            }
-
-            case 5:
-                imprime(lista);
-                break;
-
-            default:
-                printf("Opção inválida!\n");
-        }
-    } while (op != 0);
-
-    return 0;
+void liberaLista(struct desc_LE *lista) {
+    struct nodo_LDE *aux = lista->LDE;
+    while (aux != NULL) {
+        struct nodo_LDE *temp = aux;
+        aux = aux->prox;
+        free(temp->info);
+        free(temp);
+    }
+    free(lista);
 }
-
