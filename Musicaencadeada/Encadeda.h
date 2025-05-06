b3991d173f38630c75117ff25bd729cc270ad07a
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-05-05 21:37:59 -0300

    Create Encadeda.h

---
+++ Musicaencadeada/Encadeda.h
@@ -0,0 +1,12 @@
+#ifndef MUSICATAD_H
+#define MUSICATAD_H
+
+struct nodo_LDE* criaNodo(struct musica *novaMusica);
+struct desc_LDE* criaDescritor(void);
+void criaListaComDados(struct desc_LDE *lista);
+struct nodo_LDE* removeLista(struct desc_LDE *lista, int posicao);
+void imprime(struct desc_LDE *lista);
+void procurar(struct desc_LDE *lista, char nome[256]);
+void insere(struct desc_LDE *lista, struct nodo_LDE *novo_elemento, int posicao);
+
+#endif
