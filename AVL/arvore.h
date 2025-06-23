8dbb339fa6f46091d72f77608d07b5f8078f1576
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-06-23 10:17:27 -0300

    Update arvore.h

--- AVL/arvore.h
+++ AVL/arvore.h
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+typedef struct arv{
+	struct arv *esq,*dir;
+	int dado,rotacao;
+}Arv;
+
+typedef struct desc_avl{
+	struct arv *raiz;
+	int altura;
+}descritorAvl;
+
+
+descritorAvl *Cria();
+Arv *Balanceamento(Arv *no);
+void Insere(Arv **no,int valor);
+void Imprime_preorder(Arv *no);
+void Imprime_inorder(Arv *no);
+int Maior(int a, int b);
+int Altura(Arv *no);
