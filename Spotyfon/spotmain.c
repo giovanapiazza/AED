ecd89e2697669b873f32e444bd93806c4297af5f
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-06-30 10:53:34 -0300

    Update spotmain.c

--- Spotyfon/spotmain.c
+++ Spotyfon/spotmain.c
@@ -1,1 +1,51 @@
+#include <stdion.h>
+#include <string.h>
+#include <strlib.h>
 
+int main(){
+  int op;
+
+do {
+  printf("-------------------------MENU-------------------------");
+  printf("1- Carregar arquivo de músicas");
+  printf("2- Criar playlist");
+  printf("3- Buscar por uma música");
+  printf("4- Imprimir uma música");
+  printf("5- Relatório de músicas escutadas");
+  printf("6- Ver ultima música adicionada");
+  printf("7- Sair");
+  scanf("%d", &op);
+  getchar();
+
+  switch (op){
+    case 1:
+      
+      break;
+    case 2:
+    
+      break;
+    case 3:
+      
+      break;
+    case 4:
+    
+      break;
+    case 5:
+    
+      break;
+    case 6:
+    
+      break;
+    
+    case 7:
+      printf("Encerrado...");
+    break;
+
+    default:
+    printf("Opção inválida, tente novamente"); 
+    break;
+
+  }
+} while (op != 7);
+  return 0;
+}
