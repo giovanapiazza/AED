120766252aa0a37f255b7734e848332facbbb343
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-07-10 13:56:41 -0300

    Update spotmain.c

--- Spotyfon/spotmain.c
+++ Spotyfon/spotmain.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "spotyfon.h"
+#include "spot.h"
 
-int main() {
+nt main() {
     Lista listaMusicas;
     Fila playlistFila;
     Pilha playlistPilha;
-    char nomeArquivo[256];
+    char nomeArquivo[256] = "";
     int carregouArquivo = 0;
-    int opcao, op;
+    int opcao;
 
     inicializarLista(&listaMusicas);
     criarFila(&playlistFila);
@@ -21,16 +21,20 @@ int main() {
         printf("2- Criar nova playlist\n");
         printf("3- Inserir musica na playlist\n");
         printf("4- Imprimir\n");
-        printf("5- Relatorio\n");
-        printf("6- Backup playlist\n");
+        printf("5- Backup\n");
+        printf("6- Relatorio\n");
+        printf("7- Executar\n");
         printf("0- Sair\nEscolha: ");
 
-        if (scanf("%d", &opcao) != 1) { while(getchar() != '\n'); continue; }
+        if (scanf("%d", &opcao) != 1) {
+            while(getchar() != '\n');
+            continue;
+        }
         getchar();
 
         switch (opcao) {
             case 1:
-                importacao(&listaMusicas, &playlistFila, &playlistPilha, &carregouArquivo);
+                importacao(&listaMusicas, &playlistFila, &playlistPilha, &carregouArquivo, nomeArquivo);
                 break;
 
             case 2:
@@ -59,40 +63,37 @@ int main() {
                 imprimirTudo(&listaMusicas, &playlistFila, &playlistPilha);
                 break;
 
-            case 5:
+            case 5: {
+                if (playlistFila.inicio == NULL) {
+                    printf("Insira na playlist antes de salvar um backup!\n");
+                    break;
+                }
+                backup(nomeArquivo, &listaMusicas, &playlistFila, carregouArquivo);
+                break;
+            }
+
+            case 6:{
                 if (playlistFila.inicio == NULL) {
                     printf("Insira na playlist antes de salvar o relatorio!\n");
                     break;
                 }
-
-                char nomeRela[100];
+                char nomeRelatorio[100];
                 printf("Digite o nome do arquivo para salvar o relatorio (ex: relatorio.txt): ");
-                fgets(nomeRela, sizeof(nomeRela), stdin);
-                nomeRela[strcspn(nomeRela, "\n")] = 0;
+                fgets(nomeRelatorio, sizeof(nomeRelatorio), stdin);
+                nomeRelatorio[strcspn(nomeRelatorio, "\n")] = 0;
 
-                salvarRelatorio(nomeRela, &playlistFila, &listaMusicas);
+                salvarRelatorio(nomeRelatorio, &playlistFila, &listaMusicas, nomeArquivo);
                 break;
+            }
 
-            case 6:
-                if (!carregouArquivo) {
-                    printf("Voce deve carregar um arquivo primeiro.\n");
-                    break;
-                }
-                if (playlistFila.inicio == NULL && playlistFila.fim == NULL && playlistFila.tamanho == 0) {
-                    printf("A playlist ainda nao foi criada.\n");
-                    break;
-                }
-
+            case 7:
                 if (filaVazia(&playlistFila)) {
-                    printf("A playlist esta vazia. Nada a salvar.\n");
+                    printf("Playlist vazia. Insira musicas antes de executar.\n");
                 } else {
-                    char nomeback[256];
-                    printf("Digite o nome do arquivo para backup (ex: backup.txt): ");
-                    fgets(nomeback, sizeof(nomeback), stdin);
-                    nomeback[strcspn(nomeback, "\n")] = 0;
-                    salvarBackup(&playlistFila, nomeback);
+                    executarPlaylist(&playlistFila, &playlistPilha);
                 }
                 break;
+
             case 0:
                 printf("Saindo...\n");
                 break;
