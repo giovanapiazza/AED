92cc6403658e8a6f4b7f604f0f2df4f83b0806af
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-07-10 09:31:01 -0300

    Update arvore.c

--- AVL/arvore.c
+++ AVL/arvore.c
@@ -2,101 +2,164 @@
 #include <stdlib.h>
 #include "arvore.h"
 
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int altura(Arv *no) {
+    if (no == NULL) return -1;
+    return no->altura;
+}
+
 descritorAvl *Cria() {
     descritorAvl *avl = (descritorAvl *)malloc(sizeof(descritorAvl));
-    avl->altura = 0;
     avl->raiz = NULL;
     return avl;
 }
 
-Arv *Balanceamento(Arv *no) {
-    Arv *aux = NULL, *aux2 = NULL;
-    if (no != NULL) {
-        no->esq = Balanceamento(no->esq);
-        no->dir = Balanceamento(no->dir);
+// Rotação simples para a direita
+Arv *rotacaoDireita(Arv *y) {
+    Arv *x = y->esq;
+    Arv *T2 = x->dir;
 
-        if ((no->rotacao == 2) || (no->rotacao == -2)) {
-            if ((no->rotacao > 0) && (no->esq->rotacao > 0)) {
-                // Rotação Direita
-                aux = no->esq;
-                no->esq = aux->dir;
-                aux->dir = no;
-                no->rotacao = 0;
-                no = aux;
-            }
-            else if ((no->rotacao < 0) && (no->dir->rotacao < 0)) {
-                // Rotação Esquerda
-                aux = no->dir;
-                no->dir = aux->esq;
-                aux->esq = no;
-                no->rotacao = 0;
-                no = aux;
-            }
-            else if ((no->rotacao > 0) && (no->esq->rotacao < 0)) {
-                // Rotação Dupla Direita
-                aux = no->esq;
-                aux2 = aux->dir;
-                aux->dir = aux2->esq;
-                aux2->esq = aux;
-                no->esq = aux2->dir;
-                aux2->dir = no;
 
-                if (aux2->rotacao == -1)
-                    no->rotacao = 1;
-                else
-                    no->rotacao = 0;
+    x->dir = y;
+    y->esq = T2;
 
-                if (aux2->rotacao == 1)
-                    aux->rotacao = -1;
-                else
-                    aux->rotacao = 0;
 
-                no = aux2;
-            }
-            else if ((no->rotacao < 0) && (no->dir->rotacao > 0)) {
-                // Rotação Dupla Esquerda
-                aux = no->dir;
-                aux2 = aux->esq;
-                aux->esq = aux2->dir;
-                aux2->dir = aux;
-                no->dir = aux2->esq;
-                aux2->esq = no;
+    y->altura = max(altura(y->esq), altura(y->dir)) + 1;
+    x->altura = max(altura(x->esq), altura(x->dir)) + 1;
 
-                if (aux2->rotacao == -1)
-                    no->rotacao = 1;
-                else
-                    no->rotacao = 0;
-
-                if (aux2->rotacao == 1)
-                    aux->rotacao = -1;
-                else
-                    aux->rotacao = 0;
-
-                no = aux2;
-            }
-        }
-    }
-    return no;
+    return x; 
 }
 
-void Insere(Arv **no, int valor) {
-    if ((*no) == NULL) {
-        (*no) = (Arv *)malloc(sizeof(Arv));
-        (*no)->dado = valor;
-        (*no)->esq = NULL;
-        (*no)->dir = NULL;
-        (*no)->rotacao = 0;
+
+Arv *rotacaoEsquerda(Arv *x) {
+    Arv *y = x->dir;
+    Arv *T2 = y->esq;
+
+    y->esq = x;
+    x->dir = T2;
+
+    x->altura = max(altura(x->esq), altura(x->dir)) + 1;
+    y->altura = max(altura(y->esq), altura(y->dir)) + 1;
+
+    return y; 
+}
+
+// Calcula o fator de balanceamento do nó
+int fatorBalanceamento(Arv *no) {
+    if (no == NULL) return 0;
+    return altura(no->esq) - altura(no->dir);
+}
+
+// Insere  e balanceamento
+Arv *Insere(Arv *no, int valor) {
+
+    if (no == NULL) {
+        Arv *novo = (Arv *)malloc(sizeof(Arv));
+        novo->dado = valor;
+        novo->esq = novo->dir = NULL;
+        novo->altura = 0;
+        return novo;
     }
+
+
+    if (valor < no->dado)
+        no->esq = Insere(no->esq, valor);
+    else if (valor > no->dado)
+        no->dir = Insere(no->dir, valor);
+    else
+        return no; 
+
+    no->altura = max(altura(no->esq), altura(no->dir)) + 1;
+
+    int fb = fatorBalanceamento(no);
+
+    //SD
+    if (fb > 1 && valor < no->esq->dado)
+        return rotacaoDireita(no);
+
+    //SD
+    if (fb < -1 && valor > no->dir->dado)
+        return rotacaoEsquerda(no);
+
+    //DD
+    if (fb > 1 && valor > no->esq->dado) {
+        no->esq = rotacaoEsquerda(no->esq);
+        return rotacaoDireita(no);
+    }
+
+    // DE
+    if (fb < -1 && valor < no->dir->dado) {
+        no->dir = rotacaoDireita(no->dir);
+        return rotacaoEsquerda(no);
+    }
+
+    return no; 
+}
+
+// nó com menor valor 
+Arv* minValor(Arv* no) {
+    Arv* atual = no;
+    while (atual->esq != NULL)
+        atual = atual->esq;
+    return atual;
+}
+
+
+Arv* Remove(Arv* raiz, int valor) {
+    if (raiz == NULL)
+        return raiz;
+
+    // Percorre a árvore
+    if (valor < raiz->dado)
+        raiz->esq = Remove(raiz->esq, valor);
+    else if (valor > raiz->dado)
+        raiz->dir = Remove(raiz->dir, valor);
     else {
-        if (valor <= (*no)->dado) {
-            (*no)->rotacao++;
-            Insere(&(*no)->esq, valor);
-        }
-        else {
-            (*no)->rotacao--;
-            Insere(&(*no)->dir, valor);
+        // Nó com apenas um filho ou nenhum
+        if ((raiz->esq == NULL) || (raiz->dir == NULL)) {
+            Arv* temp = raiz->esq ? raiz->esq : raiz->dir;
+
+            if (temp == NULL) {
+                temp = raiz;
+                raiz = NULL;
+            } else
+                *raiz = *temp;
+
+            free(temp);
+        } else {
+            Arv* temp = minValor(raiz->dir);
+            raiz->dado = temp->dado;
+            raiz->dir = Remove(raiz->dir, temp->dado);
         }
     }
+
+    if (raiz == NULL)
+        return raiz;
+
+    // Atualiza altura e realiza balanceamento
+    raiz->altura = max(altura(raiz->esq), altura(raiz->dir)) + 1;
+    int fb = fatorBalanceamento(raiz);
+    // rotação quando remove
+    if (fb > 1 && fatorBalanceamento(raiz->esq) >= 0)
+        return rotacaoDireita(raiz);
+
+    if (fb > 1 && fatorBalanceamento(raiz->esq) < 0) {
+        raiz->esq = rotacaoEsquerda(raiz->esq);
+        return rotacaoDireita(raiz);
+    }
+
+    if (fb < -1 && fatorBalanceamento(raiz->dir) <= 0)
+        return rotacaoEsquerda(raiz);
+
+    if (fb < -1 && fatorBalanceamento(raiz->dir) > 0) {
+        raiz->dir = rotacaoDireita(raiz->dir);
+        return rotacaoEsquerda(raiz);
+    }
+
+    return raiz;
 }
 
 void Imprime_preorder(Arv *no) {
@@ -114,34 +177,3 @@ void Imprime_inorder(Arv *no) {
         Imprime_inorder(no->dir);
     }
 }
-
-Arv* Remove(Arv* no, int valor) {
-    if (no == NULL) {
-        return NULL;
-    }
-
-    if (valor < no->dado) {
-        no->esq = Remove(no->esq, valor);
-    } else if (valor > no->dado) {
-        no->dir = Remove(no->dir, valor);
-    } else {
-        if (no->esq == NULL) {
-            Arv* temp = no->dir;
-            free(no);
-            return temp;
-        } else if (no->dir == NULL) {
-            Arv* temp = no->esq;
-            free(no);
-            return temp;
-        } else {
-            Arv* temp = no->dir;
-            while (temp->esq != NULL) {
-                temp = temp->esq;
-            }
-            no->dado = temp->dado;
-            no->dir = Remove(no->dir, temp->dado);
-        }
-    }
-    return no;
-}
-
