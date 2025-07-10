51c0d2207910bfadd6099fad658cf0044fc1ab5e
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-07-10 14:34:26 -0300

    Update spot.h

--- Spotyfon/spot.h
+++ Spotyfon/spot.h
@@ -1,6 +1,8 @@
-#ifndef SPOTYFON_H
-#define SPOTYFON_H
+// spot.h
+#ifndef SPOT_H
+#define SPOT_H
 
+//---------------- STRUCTS ----------------//
 typedef struct {
     char artista[256];
     int codigo;
@@ -9,7 +11,6 @@ typedef struct {
     int execucoes;
 } Musica;
 
-// Lista encadeada
 typedef struct nodo {
     Musica musica;
     struct nodo *prox;
@@ -20,13 +21,6 @@ typedef struct {
     int tamanho;
 } Lista;
 
-void inicializarLista(Lista *lista);
-void inserirMusicaFim(Lista *lista, Musica m);
-void liberarLista(Lista *lista);
-void carregarMusicas(const char *nomeArquivo, Lista *lista);
-void imprimirMusica(const Musica *m);
-
-// Pilha
 typedef struct nodo_pilha {
     Musica musica;
     struct nodo_pilha *prox;
@@ -37,13 +31,6 @@ typedef struct {
     int tamanho;
 } Pilha;
 
-void criarPilha(Pilha *p);
-void push(Pilha *p, Musica m);
-void imprimirPilha(Pilha *p);
-void liberarPilha(Pilha *p);
-int pilhaVazia(Pilha *p)
-
-// Fila
 typedef struct nodo_fila {
     Musica musica;
     struct nodo_fila *prox;
@@ -54,24 +41,38 @@ typedef struct {
     int tamanho;
 } Fila;
 
+//---------------- FUNÇÕES LISTA ----------------//
+void inicializarLista(Lista *lista);
+void inserirMusicaFim(Lista *lista, Musica m);
+void liberarLista(Lista *lista);
+void carregarMusicas(const char *nomeArquivo, Lista *lista);
+void imprimirMusica(const Musica *m);
+
+//---------------- FUNÇÕES PILHA ----------------//
+void criarPilha(Pilha *p);
+void push(Pilha *p, Musica m);
+void imprimirPilha(Pilha *p);
+void liberarPilha(Pilha *p);
+int pilhaVazia(Pilha *p);
+
+//---------------- FUNÇÕES FILA ----------------//
 void criarFila(Fila *f);
 void enqueue(Fila *f, Musica m);
 void imprimirFila(Fila *f);
 void liberarFila(Fila *f);
 int filaVazia(Fila *f);
 
-// Operacoes principais
+//---------------- BUSCAS E OPERAÇÕES ----------------//
 int buscarMusicaLista(Lista *lista, Musica *resultado, int criterio, const char *valorStr, int valorInt);
 void buscarEInserirMusica(Lista *lista, Fila *fila, Pilha *pilha);
 void executarPlaylist(Fila *fila, Pilha *pilha);
 void salvarPlaylist(Fila *fila, const char *nomeplay);
 void imprimirTudo(Lista *lista, Fila *fila, Pilha *pilha);
 void salvarBackup(const char *nomeArquivo, Lista *lista);
-void carregarBackup(const char *nomebacktorio, Fila *fila, Pilha *pilha);
+void carregarBackup(const char *nomeArquivo, Fila *fila, Pilha *pilha);
 void importacao(Lista *lista, Fila *fila, Pilha *pilha, int *carregouArquivo, char *nomeArquivo);
 void criarPlaylist(Fila *fila, Pilha *pilha);
 void salvarRelatorio(const char *nomeRelatorio, Fila *fila, Lista *lista, const char *nomeArquivoAcervo);
 void backup(const char *nomeArquivo, Lista *lista, Fila *fila, int carregouArquivo);
-
 
 #endif
