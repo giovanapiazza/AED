99e2f984037483321dc404358edf7c325b5a4a1c
Giovana Piazza <135471104+giovanapiazza@users.noreply.github.com>
2025-07-10 09:31:18 -0300

    Update arvore.h

--- AVL/arvore.h
+++ AVL/arvore.h
@@ -6,19 +6,18 @@
 
 typedef struct arv {
     struct arv *esq, *dir;
-    int dado, rotacao;
+    int dado;
+    int altura;
 } Arv;
 
 typedef struct desc_avl {
     struct arv *raiz;
-    int altura;
 } descritorAvl;
 
 descritorAvl *Cria();
-Arv *Balanceamento(Arv *no);
-void Insere(Arv **no, int valor);
+Arv *Insere(Arv *no, int valor);
+Arv *Remove(Arv *no, int valor);
 void Imprime_preorder(Arv *no);
 void Imprime_inorder(Arv *no);
-Arv* Remove(Arv* no, int valor);
 
 #endif
